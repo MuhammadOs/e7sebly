e01e55c9a156fa71ccbad1e2de17a7b6ab890398
Yousef Mohsen Abdel Nabi Ashri <ym1667@fayoum.edu.eg>
2024-07-19 14:35:54 +0300

    HomeScreen Layout Done

--- lib/features/azkar/presentation/view/azkar.dart
+++ lib/features/azkar/presentation/view/azkar.dart
@@ -1,10 +1,48 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../../../../core/util/sizeConfig.dart';
 
 class Azkarpage extends StatelessWidget {
   const Azkarpage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    SizeConfig().init(context);
+    double w = SizeConfig.screenW!;
+    return Scaffold(
+      backgroundColor: const Color(0XFF1D6BDD),
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        toolbarHeight: 90,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon:  Icon(
+            Icons.arrow_back_ios_new_rounded,
+            color: Colors.white,
+            size: (w <= 550) ? 25 : 30,
+          ),
+        ),
+        title: Text(
+          "التسبيح والأذكار",
+          style: TextStyle(
+            fontFamily: "mess_bold",
+            color: Colors.white,
+            fontSize: (w <= 550) ? 25 : 30,
+
+          ),
+        ),
+        centerTitle: true,
+      ),
+      body:SafeArea(
+        child: Container(),
+
+      ),
+
+
+    );
   }
 }
--- lib/features/home/presentation/view/MainPage.dart
+++ lib/features/home/presentation/view/MainPage.dart
@@ -1,9 +1,8 @@
-import 'package:e7sebly/core/util/sizeConfig.dart';
+import 'package:flutter/material.dart';
 import 'package:e7sebly/features/bmi/presentation/view/bmiScreen.dart';
 import 'package:e7sebly/features/calculator/presentation/view/Calculator.dart';
 import 'package:e7sebly/features/gpa/presentation/view/Gpa.dart';
 import 'package:e7sebly/features/qrcode/presentation/view/QrCode.dart';
-import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -13,23 +12,26 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  @override
   Color blue = const Color(0xFF1e6bdd); // colors variables
   Color blue2 = const Color(0xFF2787ee);
   Color dark = const Color(0xFF0d0e22);
 
   @override
   Widget build(BuildContext context) {
-    SizeConfig().init(context);
-    double width = SizeConfig.screenW!;
-    double height = SizeConfig.screenH!;
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
+
+    final double spacing = screenWidth * 0.06;
+    final double spacingPadding = screenWidth * 0.05;// 5% of screen width for spacing
+
     return Scaffold(
-        backgroundColor: const Color(0XFF1D6BDD),
-        body: Padding(
-          padding: const EdgeInsets.only(right: 16.0,left: 16.0, top: 40),
-          child: SingleChildScrollView(
+      backgroundColor: const Color(0XFF1D6BDD),
+      body: Padding(
+        padding: EdgeInsets.only(right: spacingPadding, left: spacingPadding),
+          child: SafeArea(
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.end,
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 const Row(
                   mainAxisAlignment: MainAxisAlignment.end,
@@ -38,20 +40,20 @@ class _HomePageState extends State<HomePage> {
                       crossAxisAlignment: CrossAxisAlignment.end,
                       children: [
                         Text(
-                          'نورتنا',
+                          'أهلاً يا يوسف',
                           style: TextStyle(
                             color: Colors.white,
                             fontFamily: "mess_bold",
-                            fontSize: 50,
+                            fontSize: 40,
                           ),
                         ),
                         Text(
-                          'هتحسب إيه النهارده',
+                          'هنحسب إيه النهاردة',
                           style: TextStyle(
                             color: Colors.white,
                             fontFamily: "mess_medium",
                             letterSpacing: 1,
-                            fontSize: 24,
+                            fontSize: 18,
                           ),
                         ),
                       ],
@@ -59,7 +61,7 @@ class _HomePageState extends State<HomePage> {
                   ],
                 ),
                 const SizedBox(
-                  height: 30,
+                  height: 20,
                 ),
                 GestureDetector(
                   onTap: () {
@@ -75,22 +77,25 @@ class _HomePageState extends State<HomePage> {
                       borderRadius: BorderRadius.circular(25.0),
                       color: blue2,
                     ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        Image.asset(
-                          'assets/img/icons/barcode.png',
-                          width: 100,
-                        ),
-                        const Text(
-                          'الماسح الضوئي',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontFamily: "mess_regular",
-                            fontSize: 24,
+                    child: Padding(
+                      padding: const EdgeInsets.only(top: 12, bottom: 12),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Image.asset(
+                            'assets/img/icons/barcode.png',
+                            width: 100,
                           ),
-                        ),
-                      ],
+                          const Text(
+                            'الماسح الضوئي',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontFamily: "mess_regular",
+                              fontSize: 24,
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ),
@@ -101,186 +106,123 @@ class _HomePageState extends State<HomePage> {
                   'خدماتنا',
                   style: TextStyle(
                     color: Colors.white,
-                    fontFamily: "mess_medium",
-                    fontSize: 24,
+                    fontFamily: "mess_bold",
+                    fontSize: 28,
                   ),
                 ),
                 const SizedBox(
-                  height: 20,
+                  height: 12,
                 ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                Column(
                   children: [
-                    SizedBox(
-                      height: 160,
-                      width: 160,
-                      child: GestureDetector(
-                        onTap: () {
-                          setState(() {
-                            Navigator.pushReplacementNamed(context, "azkar");
-                          });
-                        },
-                        child: Container(
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(25.0),
-                            color: blue2,
-                          ),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: [
-                              SizedBox(
-                                width: 100,
-                                child: Image.asset(
-                                  'assets/img/icons/Exchange.png',
-                                  filterQuality: FilterQuality.high,
-                                ),
-                              ),
-                              const Text(
-                                'تحويل العملات',
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontFamily: "mess_regular",
-                                  fontSize: 20,
-                                ),
-                              ),
-                            ],
-                          ),
+                    Row(
+
+                      children: [
+                        buildServiceCard(
+                          context,
+                          'assets/img/icons/ramadan.png',
+                          'التسبيح والأذكار',
+                          blue2,
+                              () {
+                            setState(() {
+                              Navigator.pushReplacementNamed(context, "azkar");
+                            });
+                          },
                         ),
-                      ),
+                        SizedBox(width: spacing),
+                        buildServiceCard(
+                          context,
+                          'assets/img/icons/gpa.png',
+                          'المعدل التراكمي',
+                          blue2,
+                              () {
+                            setState(() {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(builder: (context) => const GPA()),
+                              );
+                            });
+                          },
+                        ),
+                      ],
                     ),
-                    SizedBox(
-                      height: 160,
-                      width: 160,
-                      child: GestureDetector(
-                        onTap: () {
-                          setState(() {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => const Calculator()),
-                            );
-                          });
-                        },
-                        child: Container(
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(28.0),
-                            color: blue2,
-                          ),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: [
-                              Image.asset(
-                                'assets/img/icons/budget.png',
-                                width: 100,
-                                filterQuality: FilterQuality.high,
-                              ),
-                              const Text(
-                                'آلة حاسبة',
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontFamily: "mess_regular",
-                                  fontSize: 20,
-                                ),
-                              ),
-                            ],
-                          ),
+                    SizedBox(height: spacing), // Dynamic spacing
+                    Row(
+                      children: [
+                        buildServiceCard(
+                          context,
+                          'assets/img/icons/budget.png',
+                          'اله حاسبة',
+                          blue2,
+                              () {
+                            setState(() {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(builder: (context) => const Calculator()),
+                              );
+                            });
+                          },
                         ),
-                      ),
+                        SizedBox(width: spacing),
+                        buildServiceCard(
+                          context,
+                          'assets/img/icons/body-mass-index.png',
+                          'كتلة الجسم',
+                          blue2,
+                              () {
+                            setState(() {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(builder: (context) => const BmiScreen()),
+                              );
+                            });
+                          },
+                        ),
+                      ],
                     ),
                   ],
-                ),
-                const SizedBox(
-                  height: 25,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    SizedBox(
-                      height: 160,
-                      width: 160,
-                      child: GestureDetector(
-                        onTap: () {
-                          setState(() {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => const BmiScreen()),
-                            );
-                          });
-                        },
-                        child: Container(
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(25.0),
-                            color: blue2,
-                          ),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [
-                              Image.asset(
-                                'assets/img/icons/body-mass-index.png',
-                                width: 100,
-                                filterQuality: FilterQuality.high,
-                              ),
-                              const Text(
-                                'كتله الجسم',
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontFamily: "mess_regular",
-                                  fontSize: 20,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ),
-                    SizedBox(
-                      height: 160,
-                      width: 160,
-                      child: GestureDetector(
-                        onTap: () {
-                          setState(() {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(builder: (context) => const GPA()),
-                            );
-                          });
-                        },
-                        child: Container(
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(25.0),
-                            color: blue2,
-                          ),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: [
-                              Image.asset(
-                                'assets/img/icons/cum-laude.png',
-                                width: 100,
-                                filterQuality: FilterQuality.high,
-                              ),
-                              const Text(
-                                'المعدل التراكمي',
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontFamily: "mess_regular",
-                                  fontSize: 20,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-                const SizedBox(
-                  height: 25,
                 ),
               ],
             ),
           ),
-        ));
+      ),
+    );
+  }
+
+  Widget buildServiceCard(BuildContext context, String imagePath, String label, Color color, VoidCallback onTap) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    final double spacing = screenWidth * 0.06;
+    final double spacingPadding = screenWidth * 0.05;// 5% of screen width for spacing
+    return SizedBox(
+      height: screenWidth-(((spacingPadding*2)*6)-(spacing/3)),
+      width: screenWidth-(((spacingPadding*2)*6)-(spacing/3)),
+      child: GestureDetector(
+        onTap: onTap,
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(25.0),
+            color: color,
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Image.asset(
+                imagePath,
+                width: 90,
+                filterQuality: FilterQuality.high,
+              ),
+              Text(
+                label,
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontFamily: "mess_regular",
+                  fontSize: 18,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
